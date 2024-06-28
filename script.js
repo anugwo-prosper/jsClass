09f654dec067ab4753dab7343dfa1e0a5169c657
Anugwo Prosper Godwin <anugwoprosper777@gmail.com>
2024-06-28 15:34:21 +0100

    prosper

---
+++ script.js
@@ -0,0 +1,692 @@
+// outputing data 
+
+// alert ("hello world");
+// confirm("ARE YOU SURE YOU WANT TO DELETE THIS");
+// document.write("HEllo world");
+// var a = prompt("what is your name");
+// console.log("HELLO WORLD");
+
+
+
+// Creation of porgram
+// let firstName=prompt("ENTER YOUR FIRST NAME");
+// let lastName=prompt("ENTER YOUR LAST NAME");
+// console.log("How are you doing"+ " "+ firstName+ " " + lastName);
+// console.log(`good morning mr. ${firstName} ${lastName}`);
+
+// let firstNumber= parseInt(prompt("Enter your first number")) ;
+// console.log(typeof(firstNumber));
+// // let secondNumber=parseInt(prompt("Enter your second number")); 
+// console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+// console.log(firstNumber + secondNumber);
+
+// let x = 30
+// --x
+// console.log(x)
+
+// let address = "34 roas k close gowon estate egbeda lagos"
+// console.log(typeof(address))
+// console.log(address[1])
+// console.log(address.slice(0,16))
+
+
+
+
+// NUMBER ONE
+// let firstName=prompt("Enter Your Name")
+// console.log(`Welcome to Digital Fotress ${firstName}`);
+
+// // NUMBER TWO
+// let firstNumber=parseInt(prompt("Enter First Number"))
+// let secondNumber=parseInt(prompt("Enter First Number"))
+// console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+
+// Number Three
+// let firstName=prompt("Enter Your First Name")
+// let secondName=prompt("Enter Your Second Name")
+// let age=prompt("Enter Your Age")
+// console.log(`Welcome to Backend class ${firstName} ${secondName} You are ${age} years old`);
+
+// Number Four
+// let favoriteColor=prompt("Enter Your Favorite Color")
+// let favoriteAnimal=prompt("Enter Your Favorite Animal")
+// console.log(`Good Day! Your favourite animal is ${favoriteAnimal} and your favourite color is ${favoriteColor} `);
+
+// Number Five
+// let firstName=prompt("Enter Your First Name")
+// let yob=parseInt(prompt("Enter Your year of birth"))
+// let currentYear=("2024")
+// console.log(`My name is ${firstName} and i am currently ${currentYear - yob}`)
+
+// Number Six
+// let homeTown=prompt("Enter Your Hometown")
+// let favoriteFood=prompt("Enter Your Favorite Food")
+// console.log(`Greetings! My Hometown is ${homeTown} and your favourite Food is ${favoriteFood} `);
+
+// Number Seven
+// let title=prompt("What Is The Book Title")
+// let author=prompt("Who is the Author")
+// console.log(`Good afternoon! I read a book titled ${title} written by ${author} `);
+
+// Number Eight
+// let cityName=prompt("What Is The Name of City Visited")
+// let yearOfVisit=prompt("What Year Did you Visit This Year")
+// console.log(`Hello There! I visited ${cityName} in the year ${yearOfVisit} `);
+
+// Number Nine
+// let favoriteMovie=prompt("What Is Your Favourite Movie?")
+// let yearOfRelease=prompt("What Year Was It Released?")
+// console.log(`My Favorite Movie is ${favoriteMovie} which was released in the year ${yearOfRelease} `);
+
+// Number Ten
+// let favoriteFruit=prompt("What Is Your Favourite Fruit?")
+// let numberOfConsumption=prompt("How many times do you consume this fruit per week?")
+// console.log(`My Favorite Fruit is ${favoriteFruit} and i eat it ${numberOfConsumption} times every week `);
+
+// Number Eleven
+// let favoriteSubject=prompt("What Is Your Favourite Subject?")
+// let gradeLevel=prompt("What is your grade level")
+// console.log(`My Favorite subject is ${favoriteSubject} and my grade level is ${gradeLevel}`);
+
+// Number Twelve
+// let favoriteSport=prompt("What Is Your Favourite Sport?")
+// let season=prompt("What season did you play it")
+// console.log(`My Favorite Sport is ${favoriteSport} and my grade level is ${season}`);
+
+
+// STRINGGGGGG
+
+// let myNew = ("Digital")
+// let address = "34 roas k close gowon estate egbeda lagos";
+// console.log(typeof(address));
+// console.log(address[1]);
+// console.log(address.slice(0,16));
+// console.log(address.concat(myNew));
+// console.log(address.lastIndexOf("s"));
+// console.log(address.toLocaleUpperCase());
+// console.log(address.toLocaleLowerCase());
+// console.log(address().reverse().join());
+// console.log(address.startsWith('1'));
+// console.log(address.endsWith('s'));
+// let newAddress = address.split();
+// // newAddress.unshift("Green");
+// newAddress.push("blue");
+// console.log(newAddress);
+// newAddress.splice(1, 3 'purple',)
+// console.log(newAddress)
+// newAddress.includes("ROAD")
+// console.log(address.includes("34"))
+// newAddress.shift()
+// console.log(newAddress)
+
+// let myNumber = 10
+// console.log(typeof(myNumber))
+// let x = myNumber.toFixed(2)
+// let x = myNumber.toPrecision(4)
+
+// ternery operator
+// console.log(myNumber > 10? "yes" : "no")
+
+// let myFloat= 10.00
+// console.log
+
+// let Tunde= Array('pink' , 'red' , 'purple' ,'blue')
+// let Color= ['pink' , 'red' , 'purple' ,'blue']
+
+// Tunde.pop()
+// Tunde.shift()
+// Tunde.reverse()
+// // Tunde[0]='Violet'     INDEXING
+// console.log(Tunde)
+
+// Color.unshift('orange')
+// Color.push('cream')
+// console.log(Color)
+
+// let Employee= [
+// {
+//     'name':'Lekan',
+//     'state':'ogun',
+//     'email':'lekan@gmail.com',
+// },
+// {
+//     'name':'prosper',
+//     'state':'imo',
+//     'email':'prosper@gmail.com',
+// },
+// {
+//     'name':'daniel',
+//     'state':'enugu',
+//     'email':'daniel@gmail.com',
+// },
+// {
+//     'name':'ola',
+//     'state':'kogi',
+//     'email':'ola@gmail.com',
+// },
+// {
+//     'name':'samuel',
+//     'state':'delta',
+//     'email':'samuel@gmail.com',
+// },
+// ]
+// let x=Employee[0]
+
+// let NEW = "Jave Script is Simple";
+
+// let words = "JavaScript is simple";
+// let script = words.match(/Script/);
+// console.log(script); 
+
+// let word 
+
+// ARITHMETIC OPERATOR +, -, /, +=, -=, %, ++, --, 
+// let num= 10
+// let num2= 5
+// console.log(num + num2)
+// console.log(num - num2)
+// console.log(num += num2)
+// console.log(num -= num2)
+// console.log(++num)
+// console.log(--num)
+
+// ARITHMETIC OPERATOR >, <, >=, <=, ==, ===, !=.
+// console.log(num > num2)
+// console.log(num < num2)
+// console.log(num >= num2)
+// console.log(num <= num2)
+// console.log(num === num2)
+
+// Logical Operator && ||
+// let country = "Nigeria"
+// let age = 18;
+
+// if( country == "Nigeria" || age >= 18){
+//     console.log("You can vote")
+// }
+// else{
+//     console.log("You cannot vote")
+// }
+
+
+// // NEWWWWW
+
+// let score= prompt("Enter your grade").toLocaleUpperCase()
+// // if(score == "A" ){
+// //     console.log("Grade A")
+// // }
+
+// switch(true){
+//     case score == "A":
+//         console.log("Grade A");
+//         break;
+//     case score == 'B':
+//         console.log("Grade B");
+//         break;
+//     case score == 'C':
+//         console.log("Grade C");
+//         break;
+//     case score == 'D':
+//         console.log("FAILURE");
+//         break;
+//     default:
+//         console.log('Unknown Result');
+// }
+
+// let myNum = Math.floor(Math.random() * 3)
+// let guess = parseInt(prompt('Enter your guess'))
+
+// if (guess == myNum){
+//     console.log("Congratulation you are correct")
+// }else if (guess > myNum){
+//     console.log("your guess is high")
+// }
+// else{
+//     console.log("INVALID")
+// }
+
+// """"FOR LOOP""""""
+// let x = 0
+// while( x < 10){
+//     x ++ 
+//     console.log(x) 
+// }
+
+// for(i = 0; i <=10; i++){
+//     console.log(i)
+// }
+// let score = [10, 20, 30, 40, 50]
+// for (i of score){
+//     if (i < 30)
+//         console.log(i)
+// 
+
+// "create a program that multiplies number using while loop"
+// let num = parseInt(prompt("enter your number"))
+// let result = 1
+// while(num <= 10){
+//     result = result * num
+//     num ++
+//     }
+
+
+
+// MULTIPLICATION TABLE
+// let x = 0 
+// let product = parseInt(prompt('enter your number'))
+// while (x <= 10){
+//     x++
+//     console.log(`${product} x ${x} = ${product * x}`)}
+
+
+// functions
+
+// const tunde = () => {
+//     console.log('good morning')
+// }
+// tunde()
+
+
+// // component function
+// const Welcome= (x, y) =>{
+// // return x * y
+// }
+// console.log( Welcome(10,20))
+
+// function greetings(){
+//     console.log('good morning')
+// }
+// greetings()
+
+// function myname( mainland , island){
+//     return `I am currently on the mainland in ${mainland}, I will be leaving to ${island} on the island`
+// }
+// console.log(myname('akowonjo', 'lekki'))
+    
+// const myname = ( mainland , island) =>{
+//     return `I am currently on the mainland in ${mainland}, I will be leaving to ${island} on the island`
+// }
+// console.log(myname('akowonjo', 'lekki'))
+
+
+// WHILE LOOP
+// let score = [10, 20, 30, 5, 50, 23, 32];
+// let num = [];
+// let n = 0;
+// while (n < 100){
+//     n++;
+//     num.push(n);
+// }
+// for(i of num){
+//     if (i % 15 == 0){
+//         console.log("FizzBuzz")
+//     }
+//     else if (i % 5 == 0){
+//         console.log("Buzz")
+//     }
+//     else if (i % 3 == 0){
+//         console.log("Fizz")
+//     }
+//     else{
+//         console.log(i)
+//     }
+
+
+// Here is the list of arithmetic operators in JavaScript:
+
+// + (Addition)
+// - (Subtraction)
+// * (Multiplication)
+// / (Division)
+// % (Remainder)
+// ++ (Increment)
+// -- (Decrement)
+// ** (Exponentiation)
+
+// ECMA SCRIPT 6 (ES6)
+// {arrow function}
+
+// let x = v => v * v
+// console.log(x(10))
+
+// let score = [20, 10, 50, 40 , 30, 17, 12, 90];
+// // higher other function
+// // mostly used in react (returns back an array depending on the function passed to it)
+// console.log(score.map((x) => x > 30));
+// // it filters numbers out based on a given condition
+// console.log(score.filter((x) => x % 2 == 0));
+// // mostly used in arithmetic calculation based on the condition given and also require two parameters
+// console.log(score.reduce((y, w) => y + w));
+// // returns the value based on the given condition (it picks one figure out)
+// console.log(score.find((x) => x > 10))
+
+// // JAVASCRIPT ----------- HTML
+// const prosper = document.getElementsByClassName('prosper')[0]
+// const prosper2 = document.getElementsByClassName('prosper')[1]
+// prosper.innerHTML = 'prosper will prosper'
+// prosper2.textContent = 'prosper will prosper'
+
+// // STYLING
+// prosper.style.backgroundColor='blue'
+// prosper.style.color='white'
+
+// // ID / CLASS
+// const godwin = document.getElementById('godwin')
+// const godwin2 = document.querySelector('.godwin')
+// godwin.innerHTML= 'godwin is here'
+// godwin2.textContent= 'godwin is here'
+
+
+// const LCU= document.querySelector('#lcu')
+// LCU.textContent = 'Lead city university'
+
+// // TAG NAME
+// const myli = document.getElementsByTagName('li')[0]
+// myli.textContent = 'myli is here'
+
+// // createElement
+// let mynew1 = document.createElement('h1')
+// mynew1.innerHTML='Welcome to DOM Javascript class'
+// document.body.appendChild(mynew1)
+
+
+
+// TODAY JAVA CLASS
+// create a bulb that switches on and off
+
+
+
+
+
+
+
+
+
+
+
+// //LOCAL STORAGE (LOCALNAME) ----  FOR SAVING DATA INTO LIBRARY
+// let myname = 'Prosper'
+// let mysave = localStorage.setItem('localName', myname)
+
+// let Employee = [
+// {'name':'Lekan',
+//     'state':'ogun',
+//     'email':'lekan@gmail.com',
+// },
+
+// {
+//     'name':'prosper',
+//     'state':'imo',
+//     'email':'prosper@gmail.com',
+// },
+// {
+//     'name':'daniel',
+//     'state':'enugu',
+//     'email':'daniel@gmail.com',
+// },
+// {
+//     'name':'ola',
+//     'state':'kogi',
+//     'email':'ola@gmail.com',
+// },
+// {
+//     'name':'samuel',
+//     'state':'delta',
+//     'email':'samuel@gmail.com',
+// },
+
+
+
+// ]
+// let people= localStorage.setItem('mypeople', JSON.stringify(Employee))
+// let myset = JSON.parse(localStorage.getItem('mypeople'))
+// console.log(myset)
+
+
+// const url = 'https://jsonplaceholder.typicode.com/posts';
+
+// const getApi = async () =>{
+// const response = await fetch (url);
+// const data = await response.json();
+// console.log(data)
+// }
+
+
+
+
+// // COLOR DISPLAY
+// unorder = document.getElementsByTagName("ul")[0];
+// unorder.style.padding = "20px";
+// unorder.style.background = "red";
+// unorder.style.display = "flex";
+// unorder.style.justifyContent = "space-between";
+// let first = document.getElementById("one");
+// first.innerHTML = "Home"
+// first.style.textDecoration = "none";
+// let second = document.getElementById("two");
+// second.innerHTML = "About"
+// second.style.textDecoration = "none";
+// let third = document.getElementById("three");
+// third.innerHTML = "Contact"
+// third.style.textDecoration = "none";
+// let fourth = document.getElementById("four")
+// fourth.textContent = "More"
+// fourth.style.textDecoration = "none";
+// let fifth = document.getElementById("five")
+// fifth.textContent = "Features"
+// fifth.style.textDecoration = "none";
+
+// let list = document.getElementsByTagName("li");
+// // For "of": it gives the element and the style [i.e it returns the element] while "in just gives the index [It returns the index position]"
+// for (let the_list of list) {
+//     // console.log(the_list)
+//     the_list.style.display = "inline";
+// }
+
+// let image = document.createElement("img");
+// image.src = "pic_bulboff.gif";
+// document.body.appendChild(image)
+
+// let body_b = document.getElementsByTagName("body")[0];
+
+// let colors = ["red", "green", "blue"];
+// let btn = document.getElementsByClassName("change-btn")[0];
+// let x = -1;
+// btn.addEventListener("click", () => {
+//     if (x >= 2){
+//         x = -1
+//     }
+//         document.body.style.backgroundColor = colors[++x];
+//         btn.innerHTML = `${colors[x]}`;
+//         console.log("e")
+//         // body_b.style.backgroundColor = "gray";
+//     }
+// );
+
+
+
+// // calculator
+// const btn_1 = document.querySelectorAll("button");
+// const my_display = document.getElementById("display")
+// btn_1.forEach((button) => {
+//     button.addEventListener("click", (Olamide) => {
+//         const prosper = Olamide.target.value
+//         console.log(prosper)
+//         if (prosper === "c"){
+//             my_display.innerHTML = '';
+//         }
+//         else if (prosper === "="){
+//             my_display.innerHTML = eval(my_display.innerHTML)
+//         }
+//         else{
+//             my_display.innerHTML += prosper
+//         }
+//     }
+//     )
+// })
+
+
+// FORM
+// let myform = document.querySelector('.myform')
+// let mydisplay = document.querySelector('.display')
+
+// myform.addEventListener('submit', (prosper) => {
+//   prosper.preventDefault();
+//   console.log('form is ready')
+//   let myname = myform.querySelector('input[name=name]').value;
+//   mydisplay.innerHTML = myname;
+// })
+
+
+
+// create a button displaying a message when clicked on
+// const myButton = document.createElement('button');
+// myButton.innerHTML = 'Click me!';
+// document.body.appendChild(myButton);
+
+// let paragraph = document.createElement('p')
+// paragraph.innerHTML = ''
+// document.body.appendChild(paragraph)
+
+
+// myButton.addEventListener('click', () => {
+//    paragraph.textContent = 'javascript is simple'
+//   });
+
+
+
+// let value = 0;
+
+// const increaseBtn = document.createElement("button");
+// document.body.appendChild(increaseBtn);
+// increaseBtn.textContent = "+";
+
+
+// const decreaseBtn = document.createElement("button");
+// document.body.appendChild(decreaseBtn);
+// decreaseBtn.textContent = "-";
+
+// const result = document.createElement("p");
+// result.textContent = value;
+// document.body.appendChild(result);
+
+
+// increaseBtn.addEventListener("click", () => {
+//   value++;
+//   result.textContent = value;
+// });
+
+// decreaseBtn.addEventListener("click", () => {
+//   value--;
+//   result.textContent = value;
+// });
+
+
+// create two images that switches between themselves with the use of a button
+
+// let button = document.createElement('button')
+// button.textContent = "click me"
+// document.body.appendChild(button)
+
+// let imageOne = document.createElement('img')
+// imageOne.addEventListener("img1", ()=> {
+// imageOne.src = "pic_bulboff.gif"
+
+// })
+
+// let imageTwo = document.createElement('img')
+
+// let data = [
+// {
+//     id: 1,
+//     name: "Prosper",
+//     email: 'anugwoprosper777@gmail.com'
+// },
+// {
+//     id: 1,
+//     name: "Anugwo",
+//     email: 'anugwoprosper777@gmail.com'
+// },
+// {
+//     id: 1,
+//     name: "Izunna",
+//     email: 'anugwoprosper777@gmail.com'
+// },
+// ]
+
+// function readAll(){
+//     localStorage.setItem('object', JSON.stringify(data))
+//     var propser = document.querySelector('.data_table')
+//     var myObject = localStorage.getItem('object')
+//     var myobjectData = JSON.parse(myObject)
+//     // console.log{myobjectData}
+//     var element = "";
+//     myobjectData.map(x =>(
+//         element += `
+//         <tr>
+//         <td>${x.id}</td>
+//         <td>${x.name}</td>
+//         <td>${x.email}</td> 
+//         <td><button>EDIT</button></td> 
+//         <td><button>DELETE</button></td> 
+//         </tr>
+//         `
+//     ))
+//     propser.innerHTML= element;
+// }
+
+// // readAll()
+
+// function mycreate(){
+//     document.querySelector('.create_form').style.display='block'
+//     document.querySelector('.add_div').style.display='none'
+// }
+
+// function myadd(){
+//     var name= document.querySelector('.name').value;
+//     var email= document.querySelector('.email').value;
+//     let myclass = math.floor(math.random() * 1000)
+//     var myobject = {id: 5, name:name, email:email};
+//     data.push(myobject)
+//     document.querySelector('.create_form').style.display='none';
+//     document.querySelector('.add_div').style.display='block';
+//     readAll()
+
+// }
+
+
+
+// OBJECT ORIENTED PROGRAMMING
+// class Product{
+//     constructor(name, brand, color, version){
+//         this.name = name;
+//         this.brand = brand;
+//         this.color = color;
+//         this.version = version;
+//     }
+
+//     getProduct(){
+//         console.log(`The current product has the features of ${this.name} ${this.brand} ${this.color} ${this.version}`)
+//     }
+// }
+
+// let myProduct = new Product('Headset', 'Oraimo', 'Black', '2.2')
+// console.log(myProduct.getProduct())
+
+// class NewProduct extends Product{
+//     constructor(name, brand, color, version, model, price){
+//         super(name, brand, color, version)
+//         this.model = model
+//         this.price = price
+//     }
+//     myNewProduct(){
+//         console.log(`The new product has the features of ${this.name} ${this.brand} ${this.color} ${this.version} ${this.model} ${this.price}`)
+// }
+// }
+
+// let myProduct2 = new NewProduct('Headset', 'Oraimo', 'Black', '2.2', 'Samsung', 12000)
+// console.log(myProduct2.myNewProduct())
+
+
+
+
